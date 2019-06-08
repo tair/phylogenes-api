df62bbfb30889902c9001128980ad76e20846d28
Qian Li <qianli1987@arabidopsis.org>
2019-06-07 18:05:50 -0700

    PHG-153 added libs for production

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const logger = require('./startup/logging');
+const config = require('config');
 const express = require('express');
 const app = express();
 
@@ -6,6 +7,9 @@ require('./startup/exception')();
 require('./startup/config')();
 require('./startup/solr').ping();
 require('./startup/routes')(app);
+if (config.get('NODE_ENV')=='production'){
+    require('./startup/prod')(app);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
