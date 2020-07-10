becfaa84c2ab6a28092c903abff5db1f797081d0
Swapnil Sawant <swapnil.sawant@arabidopsis.org>
2020-07-10 13:04:48 -0700

    change port to 80

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ require('./startup/exception')();
 require('./startup/config')();
 require('./startup/solr').ping();
 require('./startup/routes')(app);
-if (process.env.NODE_ENV=='production'){
+if (process.env.NODE_ENV == 'production') {
     require('./startup/prod')(app);
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
