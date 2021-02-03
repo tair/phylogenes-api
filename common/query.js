858a9ff0df286090727fe8039db70246b05c11b6
Swapnil Sawant <swapnil.sawant@arabidopsis.org>
2021-02-03 14:24:43 -0800

    add taxonomic ranges to facet field

--- common/query.js
+++ common/query.js
@@ -1,6 +1,6 @@
 function buildGeneralQuery(searchText) {
     var q = "";
-    if(searchText != null && searchText != "") {
+    if (searchText != null && searchText != "") {
         q += " AND exactMatch:\"" + searchText + "\"^10";
         // q += " OR gene_symbols:\"" + searchText + "\""; //phg-83
         q += " OR family_name:\"" + searchText + "\"~10";
@@ -9,16 +9,21 @@ function buildGeneralQuery(searchText) {
         q += " OR uniprot_ids:\"" + searchText + "\" ";
     }
     q = q.substr(5);
-    if(q == "")
+    if (q == "")
         q = "*:*";
     return q;
 }
 
-function buildFieldQuery(filters){
+function buildFieldQuery(filters) {
     let fqObj = {};
     const fq = []
+
     for (var i = 0; i < filters.species.length; i++) {
         fqObj = { field: 'species_list', value: `"${filters.species[i]}"` };
+        fq.push(fqObj);
+    }
+    for (var i = 0; i < filters.species.length; i++) {
+        fqObj = { field: 'taxonomic_ranges', value: `"${filters.species[i]}"` };
         fq.push(fqObj);
     }
     for (var i = 0; i < filters.organisms.length; i++) {
@@ -28,11 +33,11 @@ function buildFieldQuery(filters){
     return fq;
 }
 
-function buildFacetQuery(facet){
+function buildFacetQuery(facet) {
     let facetQuery = '';
     if (facet == 'on') {
         // facetQuery = 'facet.field=node_types&facet.field=organisms&facet.field=species_list&facet=on';
-        facetQuery  = 'facet.field=organisms&facet.field=species_list&facet=on';
+        facetQuery = 'facet.field=organisms&facet.field=species_list&facet.field=taxonomic_ranges&facet=on';
     }
     return facetQuery;
 }
